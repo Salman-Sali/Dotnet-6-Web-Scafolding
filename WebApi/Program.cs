9ff9960a06814718b7a68de02d6de56e23235e93
Salman-Sali <87238284+Salman-Sali@users.noreply.github.com>
2022-09-30 21:32:08 +0530

    program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,17 @@
+using FluentValidation;
+using Infrastructure.Context;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Shared;
+using Shared.Configrations;
+using System.Reflection;
+using WebApp.Extensions;
+using WebApp.Extentions;
+using WebApp.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,9 +21,50 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+#region Services
+var services = builder.Services;
+
+services.AddMediatR(typeof(GetAllEmployeeTypesQuery).Assembly);
+AssemblyScanner.FindValidatorsInAssembly(typeof(GetAllEmployeeTypesQuery).Assembly)
+.ForEach(item => services.AddScoped(item.InterfaceType, item.ValidatorType));
+services.AddMediatR(typeof(AddEmployeeTypeCommand).Assembly);
+AssemblyScanner.FindValidatorsInAssembly(typeof(AddEmployeeTypeCommand).Assembly)
+.ForEach(item => services.AddScoped(item.InterfaceType, item.ValidatorType));
+
+services.AddScoped(typeof(IPipelineBehavior<,>), typeof(MediatRValidationPipelineBehavior<,>));
+services.AddMediatR(typeof(ServiceCollectionExtensions).GetTypeInfo().Assembly);
+services.AddHttpContextAccessor();
+
+IConfiguration Configuration = builder.Configuration;
+
+var appSettings = Configuration.Load<AppSettings>();
+services.AddSingleton((provider) =>
+{
+    return appSettings;
+});
+
+services.AddDbContext<AppDbContext>((provider, options) =>
+{
+    options.UseNpgsql(appSettings.DbConnectionString);
+    options.EnableSensitiveDataLogging(false);
+});
+services.AddScoped<IUser, User>();
+
+var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+services.AddMvc(options =>
+{
+    options.Filters.Add(new AuthorizeFilter(policy));
+});
+#endregion
+
+
+
+#region app
+var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -23,3 +78,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+#endregion
